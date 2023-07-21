7fe8e04d8b02296c3b84fb5b4a8feeeb0d6850df
krups17 <krupaprajapati17@gmail.com>
2023-07-21 14:53:46 -0400

    board ctor

--- board.cc
+++ board.cc
@@ -2,3 +2,13 @@
 #include "board.h"
 
 using namespace std;
+
+Board::Board(vector<int, int> layout[], int vertices[], int edges[]) {
+    int subArraySize = 6;
+    int numCalls = 19;
+
+    for (int i = 0; i < numCalls; ++i) {
+        int index = i * (subArraySize + 1);
+        Tile(i, layout[i], vertices[index], edges[index], subArraySize);
+    }
+}
