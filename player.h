32ee48f3a0e4f2f7744f17f908fc47e0eebbf946
Liz Loubenski <132858049+loubenl@users.noreply.github.com>
2023-07-21 01:02:07 -0400

    Update player.h

--- player.h
+++ player.h
@@ -6,7 +6,6 @@
 #include <vector>
  
 class Player {
-<<<<<<< HEAD
     Color color;
     int buildingPoints = 0, resocTotal = 0;
     std::map<Resource, int> resocMap;
@@ -16,13 +15,7 @@ class Player {
     Player(Color color, int buildingPoints, int resocTotal, std::map<Resource, 
                                 int> resocMap, std::vector<int> occupiedTiles);
     
-    
     void roll();
-
-=======
-    int points;
-    Player(int points): points{points} {}
->>>>>>> 27eb0a4234634e9715d927796c1ab7fdb8ff866f
 
 };
 
