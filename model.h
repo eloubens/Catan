dadeaf76a9dc53526413ce5e66e5ac858e7f705a
krups17 <krupaprajapati17@gmail.com>
2023-07-21 19:47:51 -0400

    settlement map

--- model.h
+++ model.h
@@ -21,15 +21,29 @@ enum class Settlement {
 class Model {
     // stores how many resorces each settlement costs
     // SET MAP HERE (not in ctor)
-    std::map<Settlement, std::map<Resource,int>> settlementCost;
-    //= {
-        //{B, }, 
-        //{H, },
-        //{T, }, 
-        //{R, }
-
-
-    //}
+    std::map<Settlement, std::map<Resource,int>> settlementCost = {
+        {Settlement::B, {
+            {Resource::BRICK, 1},
+            {Resource::GLASS, 1},
+            {Resource::ENERGY, 1},
+            {Resource::WIFI, 1}
+        }},
+        {Settlement::H, {
+            {Resource::GLASS, 2},
+            {Resource::HEAT, 3}
+        }},
+        {Settlement::T, {
+            {Resource::BRICK, 3},
+            {Resource::ENERGY, 2}
+            {Resource::GLASS, 2},
+            {Resource::WIFI, 1},
+            {Resource::HEAT, 2}
+        }}, 
+        {Settlement::R, {
+            {Resource::HEAT, 1},
+            {Resource::WIFI, 1}
+        }}
+    }
 
 
 
