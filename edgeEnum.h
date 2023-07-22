40563fa01fa3629eada5ea3e166d131f30071e61
k6prajap <k6prajap@uwaterloo.ca>
2023-07-21 21:27:36 -0400

    enums

---
+++ edgeEnum.h
@@ -0,0 +1,13 @@
+#ifndef EDGRENUM_H
+#define EDGEENUM_H
+
+enum class edgeEnum {
+    T = 0, // top
+    TL = 1, // top left
+    TR = 2, // top right
+    BL = 3, // bottom left
+    BR = 4, // bottom right
+    B = 5 // bottom 
+};
+
+#endif
