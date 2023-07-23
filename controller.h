5268717ba1c0a0757126b80b6198d51415a857b6
Liz <eloubens@uwaterloo.ca>
2023-07-23 19:10:17 -0400

    m

--- controller.h
+++ controller.h
@@ -9,18 +9,17 @@
 #include "view.h"
 
 class Controller {
-    int argc; 
-    char **argv; 
     std::ostream &out = std::cout;
     std::istream &in = std::cin;
+    std::ostream &err = std::cerr;
     Color turn = Color::B; // Blue goes first 
     std::unique_ptr<Model> model; // default ctor initializes to nullptr
     std::unique_ptr<View> view; // default ctor initializes to nullptr
 
     void roll(Color turn);
     void trade();
-    void generateRandomBoard();// change up
-
+    // sets the Model field of the controller. Loads a board from a file, creates and loads a randomized board, or loads a saved game.
+    void setModel(bool randomize, std::ostringstream &board_oss, unsigned &seed, vector<string> &arg_vec);
     // more functions
  public: 
     Controller(int argc, char * argv []);
