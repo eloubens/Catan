4bbe465bb4d2b9fc13ed7a2c165e4ddd1b9a655f
Liz <eloubens@uwaterloo.ca>
2023-07-31 00:25:23 -0400

    m

--- model.cc
+++ model.cc
@@ -361,7 +361,7 @@ bool Model::enoughResoc(string curPlayer, string give) {
 
 bool Model::validSteal(string tradePlayer, string take) {
     for (auto p : players) {
-        if (getColorStr(p.getColour()) == tradePlayer || getColorChar(p.getColour()) == tradePlayer) {
+        if (getColorStr(p.getColour()) == tradePlayer) {
             return p.validSteal(take);
         }
     }
